7fb9e0ba412117db0fcd25a1783d561de35bbaae
Andrew Trettel <atrettel@users.noreply.github.com>
2020-11-05 08:46:59 -0500

    feat: add empty processing scripts for remaining studies

---
+++ src/E1929001.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2020 Andrew Trettel
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This file is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this file.  If not, see <https://www.gnu.org/licenses/>.
+
+import sqlite3
+import sheardata as sd
+import sys
+
+conn = sqlite3.connect( sys.argv[1] )
+cursor =  conn.cursor()
+cursor.execute( "PRAGMA foreign_keys = ON;" )
+
+flow_class   = sd.EXTERNAL_FLOW_CLASS
+year         = 1929
+study_number = 1
+
+study_identifier = sd.add_study(
+    cursor,
+    flow_class=flow_class,
+    year=year,
+    study_number=study_number,
+    study_type=sd.EXPERIMENTAL_STUDY_TYPE,
+)
+
+sd.add_source( cursor, study_identifier, "StantonTE+1929+eng+RPRT", 1 )
+
+conn.commit()
+conn.close()
