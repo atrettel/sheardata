a5a2d227a95a5ec3d4a631061aa38a2ec30f5e84
Andrew Trettel <atrettel@users.noreply.github.com>
2020-11-12 12:57:21 -0500

    feat: plot pipe flow velocity ratios as a function of bulk Reynolds number

--- src/post_pipe_flow_velocity_ratios.py
+++ src/post_pipe_flow_velocity_ratios.py
@@ -17,8 +17,15 @@
 import sheardata as sd
 import sqlite3
 import sys
+import numpy as np
 from uncertainties import ufloat
 from uncertainties import unumpy as unp
+
+import matplotlib as mpl
+mpl.use("pgf")
+import matplotlib.pylab as plt
+import grfstyl as gfx
+mpl.rcParams.update( gfx.rc_custom_preamble() )
 
 conn   = sqlite3.connect( sys.argv[1] )
 cursor = conn.cursor()
@@ -90,9 +97,49 @@ stations = []
 for result in cursor.fetchall():
     stations.append( result[0] )
 
+bulk_reynolds_number_array = []
+velocity_ratio_array       = []
 for station in stations:
-    print( station )
+    bulk_reynolds_number_array.append( sd.get_station_value(
+        cursor,
+        station,
+        sd.BULK_REYNOLDS_NUMBER_QUANTITY,
+        averaging_system=sd.UNWEIGHTED_AVERAGING_SYSTEM,
+    ) )
+    velocity_ratio_array.append( sd.get_station_value(
+        cursor,
+        station,
+        sd.BULK_TO_CENTER_LINE_VELOCITY_RATIO_QUANTITY,
+        averaging_system=sd.UNWEIGHTED_AVERAGING_SYSTEM,
+    ) )
 
 conn.commit()
 conn.close()
 
+fig = plt.figure()
+ax  = fig.add_subplot( 1, 1, 1 )
+
+bulk_reynolds_number = np.array( bulk_reynolds_number_array )
+velocity_ratio       = np.array( velocity_ratio_array       )
+
+bulk_reynolds_number_bounds = ( 1.00e1, 1.00e6 )
+velocity_ratio_bounds       = ( 0.50,   0.85   )
+
+ax.semilogx(
+    unp.nominal_values( bulk_reynolds_number  ),
+    unp.nominal_values( velocity_ratio ),
+    marker="o",
+    linestyle="",
+    clip_on=False,
+)
+
+ax.set_xlim( bulk_reynolds_number_bounds )
+ax.set_ylim(       velocity_ratio_bounds )
+
+gfx.label_axes(
+    ax,
+    r"$\mathrm{Re}_b$",
+    r"$\frac{U_b}{U_c}$",
+)
+
+fig.savefig( "figure-pipe-flow-velocity-ratios.pgf" )
