effaa600b8b67301ce420a32dfd3a29e19ce4ff8
Andrew Trettel <atrettel@users.noreply.github.com>
2020-11-12 13:05:33 -0500

    refactor: adjust Reynolds number bounds in pipe flow velocity ratio figure

--- src/post_pipe_flow_velocity_ratios.py
+++ src/post_pipe_flow_velocity_ratios.py
@@ -122,7 +122,7 @@ ax  = fig.add_subplot( 1, 1, 1 )
 bulk_reynolds_number = np.array( bulk_reynolds_number_array )
 velocity_ratio       = np.array( velocity_ratio_array       )
 
-bulk_reynolds_number_bounds = ( 1.00e1, 1.00e6 )
+bulk_reynolds_number_bounds = ( 1.00e3, 1.00e5 )
 velocity_ratio_bounds       = ( 0.50,   0.85   )
 
 ax.semilogx(
