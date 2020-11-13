b6d0dd9d1439a6c18957f990dd46ce94f8790464
Andrew Trettel <atrettel@users.noreply.github.com>
2020-11-12 20:58:41 -0500

    docs: add relevant quotes with data to script for D1947001

--- src/pre_D1947001.py
+++ src/pre_D1947001.py
@@ -38,3 +38,73 @@ sd.add_source( cursor, study_identifier, "HuebscherRG+1947+eng+JOUR", 1 )
 
 conn.commit()
 conn.close()
+
+# Duct dimensions
+#
+# p. 128
+#
+# \begin{quote}
+# The first part of the paper gives the results of an experimental
+# investigation using three ducts of different forms but each of 8 in.
+# equivalent diameter.  The duct sizes were 8 in. ID round, 8 in. square and
+# 4.5 in. by 36 in. rectangular (8:1 aspect ratio).  Air velocities used ranged
+# from 300 to 9310 fpm.
+# \end{quote}
+
+# Duct material
+#
+# p. 128
+#
+# \begin{quote}
+# The three ducts were fabricated from 16 gage galvanized sheet metal to
+# provide the necessary rigidity against deflection.
+# \end{quote}
+#
+# p. 129
+#
+# \begin{quote}
+# The internal roughness of all three ducts was typical of galvanized iron,
+# very little roughness was contributed by the joints.  The hydraulic roughness
+# magnitude cannot be measured geometrically but can be deduced from the test
+# results.
+# \end{quote}
+
+# p. 128
+#
+# \begin{quote}
+# The mean air velocity was determined from the measurement of the air quantity
+# and the duct area.  \ldots  Air quantity was measured by the use of five cast
+# aluminum nozzles made approximately to ASME log-radius, low-ratio proportions
+# and equiped with throat static taps.  \ldots  The nozzles were calibrated in
+# place by impact tube traverses at the throat over the full flow range.
+# \end{quote}
+volume_flow_rate_measurement_technique = sd.IMPACT_TUBE_MEASUREMENT_TECHNIQUE
+
+# Uncertainty of flow rate measurements
+#
+# p. 128
+#
+# \begin{quote}
+# The estimated error in any flow measurement due to all sources, including the
+# assumption of constant nozzle coefficient, did not exceed $\pm 2$ percent.
+# \end{quote}
+
+# p. 129
+wall_shear_stress_measurement_technique = sd.PRESSURE_DROP_MEASUREMENT_TECHNIQUE
+
+# Uncertainty of wall shear stress measurements
+#
+# p. 129
+#
+# \begin{quote}
+# The maximum sensitivity of the five gages was $\pm 0.02$ in. of water, with
+# an accuracy within this value over the entire range.
+# \edn{quote}
+
+# p. 132
+#
+# \begin{quote}
+# Two traverses at opposite ends of the round duct indicated different velocity
+# profiles, the centerline velocity at the downstream end was 6.6 percent
+# higher than at the upstream end for a mean velocity of 9310 fpm.
+# \end{quote}
