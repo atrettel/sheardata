74460e89964bdc884254faa058319be1d837b3a6
Andrew Trettel <atrettel@users.noreply.github.com>
2021-10-30 19:03:18 -0400

    feat: create empty script for study B1914001

---
+++ src/pre_B1914001.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2021 Andrew Trettel
+#
+# SPDX-License-Identifier: MIT
+
+import csv
+import math
+import sqlite3
+import sheardata as sd
+import sys
+
+conn   = sqlite3.connect( sys.argv[1] )
+cursor = conn.cursor()
+cursor.execute( "PRAGMA foreign_keys = ON;" )
+
+flow_class   = sd.BOUNDARY_LAYER_FLOW_CLASS
+year         = 1914
+study_number = 1
+
+study_identifier = sd.add_study(
+    cursor,
+    flow_class=flow_class,
+    year=year,
+    study_number=study_number,
+    study_type=sd.EXPERIMENTAL_STUDY_TYPE,
+)
+
+sd.add_source( cursor, study_identifier, "RiabouchinskyD+1914+fra+JOUR", 1 )
+
+conn.commit()
+conn.close()
