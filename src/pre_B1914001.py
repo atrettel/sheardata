c271b1d6a6fd41990a2b1b602fa980e5540b3270
Andrew Trettel <atrettel@users.noreply.github.com>
2021-10-31 19:11:00 -0400

    feat: start work adding profiles from study B1914001 to database

--- src/pre_B1914001.py
+++ src/pre_B1914001.py
@@ -35,6 +35,113 @@ station_4_velocity_note = sd.add_note(
     "../data/{:s}/note_station_4_velocity.tex".format( study_identifier ),
 )
 
+galilean_transformation_note = sd.add_note(
+    cursor,
+    "../data/{:s}/note_galilean_transformation.tex".format( study_identifier ),
+)
+
+series_number = 1
+series_identifier = sd.add_series(
+    cursor,
+    flow_class=flow_class,
+    year=year,
+    study_number=study_number,
+    series_number=series_number,
+    number_of_dimensions=2,
+    coordinate_system=sd.RECTANGULAR_COORDINATE_SYSTEM,
+    notes=[galilean_transformation_note,],
+)
+
+freestream_velocity = sd.sdfloat(16.0)
+
+sd.set_series_value( cursor, series_identifier, sd.Q_FREESTREAM_VELOCITY, freestream_velocity, )
+
+sd.update_series_geometry(
+    cursor,
+    series_identifier,
+    sd.RECTANGULAR_GEOMETRY
+)
+
+globals_filename = "../data/{:s}/globals.csv".format( study_identifier )
+with open( globals_filename, "r" ) as globals_file:
+    globals_reader = csv.reader( globals_file, delimiter=",", quotechar='"', \
+        skipinitialspace=True )
+    next(globals_reader)
+    for globals_row in globals_reader:
+        station_number = int(globals_row[0])
+        x              = sd.sdfloat(globals_row[1])
+        z              = sd.sdfloat(0.0)
+
+        station_identifier = sd.add_station(
+            cursor,
+            flow_class=flow_class,
+            year=year,
+            study_number=study_number,
+            series_number=series_number,
+            station_number=station_number,
+        )
+
+        # Add in previous and next stations.
+
+        station_filename = "../data/{:s}/station_{:d}.csv".format(
+            study_identifier,
+            station_number,
+        )
+
+        point_number = 0
+        y = []
+        u = []
+        with open( station_filename, "r" ) as station_file:
+            station_reader = csv.reader( station_file, delimiter=",", \
+                quotechar='"', skipinitialspace=True )
+            next(station_reader)
+            for station_row in station_reader:
+                point_number += 1
+
+                point_label = None
+                if ( point_number == 1 ):
+                    point_label = sd.WALL_POINT_LABEL
+
+                point_identifier = sd.add_point(
+                    cursor,
+                    flow_class=flow_class,
+                    year=year,
+                    study_number=study_number,
+                    series_number=series_number,
+                    station_number=station_number,
+                    point_number=point_number,
+                    point_label=point_label,
+                )
+
+                y = sd.sdfloat( float(station_row[0]) )
+                u = freestream_velocity - sd.sdfloat( float(station_row[1]) )
+                w = sd.sdfloat( 0.0 )
+
+                outer_layer_velocity = u / freestream_velocity
+
+                current_notes = []
+                if ( station_number == 4 and point_number == 10 ):
+                    current_notes = [station_4_velocity_note]
+
+                sd.set_point_value( cursor, point_identifier, sd.Q_DISTANCE_FROM_WALL,     y, )
+                sd.set_point_value( cursor, point_identifier, sd.Q_STREAMWISE_COORDINATE,  x, )
+                sd.set_point_value( cursor, point_identifier, sd.Q_TRANSVERSE_COORDINATE,  y, )
+                sd.set_point_value( cursor, point_identifier, sd.Q_SPANWISE_COORDINATE,    z, )
+                sd.set_point_value( cursor, point_identifier, sd.Q_STREAMWISE_VELOCITY,    u,                    averaging_system=sd.BOTH_AVERAGING_SYSTEMS, measurement_techniques=[sd.MT_PITOT_STATIC_TUBE], notes=current_notes, )
+                sd.set_point_value( cursor, point_identifier, sd.Q_SPANWISE_VELOCITY,      w,                    averaging_system=sd.BOTH_AVERAGING_SYSTEMS, measurement_techniques=[sd.MT_ASSUMPTION],                             )
+                sd.set_point_value( cursor, point_identifier, sd.Q_OUTER_LAYER_VELOCITY,   outer_layer_velocity, averaging_system=sd.BOTH_AVERAGING_SYSTEMS, measurement_techniques=[sd.MT_PITOT_STATIC_TUBE],                      )
+
+                for quantity in [ sd.Q_ROUGHNESS_HEIGHT,
+                                  sd.Q_INNER_LAYER_ROUGHNESS_HEIGHT,
+                                  sd.Q_OUTER_LAYER_ROUGHNESS_HEIGHT, ]:
+                    sd.set_labeled_value(
+                        cursor,
+                        station_identifier,
+                        quantity,
+                        sd.WALL_POINT_LABEL,
+                        sd.sdfloat(0.0),
+                        measurement_techniques=[sd.MT_ASSUMPTION],
+                    )
 
 conn.commit()
 conn.close()
