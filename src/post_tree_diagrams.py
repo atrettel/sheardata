563bd0ccb9b164e74939c19786d7f0109f118a1d
Andrew Trettel <atrettel@users.noreply.github.com>
2021-11-26 18:54:44 -0500

    feat: place end nodes at same rank in flow classification tree diagram

--- src/post_tree_diagrams.py
+++ src/post_tree_diagrams.py
@@ -13,10 +13,10 @@ cursor.execute( "PRAGMA foreign_keys = ON;" )
 
 with open( "figure-flow-classification-tree-diagram.gv", "w" ) as f:
     f.write( 'digraph taxonomy {\n' )
-    f.write( 'concentrate=true\n' )
-    f.write( 'rankdir=LR\n' )
-    f.write( 'ranksep="0.25"\n' )
-    f.write( 'node [shape=circle, fixedsize=true, width="0.5", height="0.5", margin="0.25"]\n' )
+    f.write( 'concentrate=true;\n' )
+    f.write( 'rankdir=LR;\n' )
+    f.write( 'ranksep="0.25";\n' )
+    f.write( 'node [shape=circle, fixedsize=true, width="0.5", height="0.5", margin="0.25"];\n' )
 
     cursor.execute(
     """
@@ -29,7 +29,7 @@ with open( "figure-flow-classification-tree-diagram.gv", "w" ) as f:
         f.write( '"{:s}" [texlbl="{:s}"]'.format(
             result[0],
             r"\texttt{"+result[0]+r"}",
-        )+"\n" )
+        )+";\n" )
     
     cursor.execute(
     """
@@ -44,6 +44,8 @@ with open( "figure-flow-classification-tree-diagram.gv", "w" ) as f:
             result[0],
             result[1],
         )+"\n" )
+
+    f.write( '{rank = same; F; I; E;}\n' )
 
     f.write( '}\n' )
 
